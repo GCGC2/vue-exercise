4818b93ac57c5af8c41521653995fdc9df0abfcb
GCGC2 <874436099@qq.com>
2021-01-21 18:18:59 +0800

    没怎么写

--- demo.js
+++ demo.js
@@ -1,10 +1,10 @@
 const vm = new Vue({
-    el: "#app",
-    data: {
-        msg: "哈哈"
+    el:"#app",
+    data:{
+        msg:"亲爱的阿蔡"
     }
-});
-// vm.$mount("#app");
-
-
+})
+// console.log(vm.data.msg);
+console.log(vm.msg);
+vm.msg = "阿蔡爱郭郭"
 
