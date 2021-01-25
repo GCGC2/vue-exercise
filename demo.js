ba532b7e37d4c017dd4e78308dfac981ea7ff7ca
GCGC2 <874436099@qq.com>
2021-01-25 15:24:10 +0800

    v-bind

--- demo.js
+++ demo.js
@@ -1,23 +1,6 @@
-// const vm = new Vue({
-//   el: '#app',
-//   data: {
-//     cFamily: ['阿蔡', '小蔡', '蔡娃'],
-//     html:"<span>阿蔡和郭郭相亲相爱</span>"
-//   }
-// })
-
-
-// v-html
 const vm = new Vue({
   el: '#app',
   data: {
-    msg: 'hello world'
+    type: "C"
   }
 })
-
-const oInput = document.getElementsByTagName('input')[0];
-const oButton = document.getElementsByTagName('button')[0];
-let msg = null;
-oButton.onclick = function () {
-  vm.msg = oInput.value; //使值变更为输入的值
-}
