b4455ae847bbab85625c27af67010791e52f4ffa
GCGC2 <874436099@qq.com>
2021-01-25 13:13:22 +0800

    一些指令

--- demo.js
+++ demo.js
@@ -1,12 +1,23 @@
+// const vm = new Vue({
+//   el: '#app',
+//   data: {
+//     cFamily: ['阿蔡', '小蔡', '蔡娃'],
+//     html:"<span>阿蔡和郭郭相亲相爱</span>"
+//   }
+// })
+
+
+// v-html
 const vm = new Vue({
   el: '#app',
   data: {
-    cFamily: ['阿蔡', '小蔡', '蔡娃']
+    msg: 'hello world'
   }
 })
-// vm.cFamily[3] = '郭妹儿';
 
-// vm.cFamily.push("郭妹儿")
-
-vm.$set(vm.cFamily, 3, "小郭妹儿")
-
+const oInput = document.getElementsByTagName('input')[0];
+const oButton = document.getElementsByTagName('button')[0];
+let msg = null;
+oButton.onclick = function () {
+  vm.msg = oInput.value; //使值变更为输入的值
+}
